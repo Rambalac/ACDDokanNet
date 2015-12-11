af71c3abc6c68dad3834570f721a30d752663784
Rambalac <rambalac+github@gmail.com>
2015-12-11 17:12:22 +0900

    Updated readme. Fixed forgotten files.

---
+++ AmazonCloudDriveApi/AmazonNodes.cs
@@ -0,0 +1,70 @@
+﻿using AmazonCloudDriveApi;
+using Azi.Amazon.CloudDrive.Json;
+using Azi.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Azi.Amazon.CloudDrive
+{
+
+    public class AmazonNodes
+    {
+        private readonly AmazonDrive amazon;
+        HttpClient json => amazon.http;
+        static TimeSpan generalExpiration => AmazonDrive.generalExpiration;
+
+        public AmazonNodes(AmazonDrive amazonDrive)
+        {
+            this.amazon = amazonDrive;
+        }
+
+        public async Task<AmazonChild> GetNode(string id)
+        {
+            var url = "{0}/nodes/{1}";
+            var result = await json.GetJsonAsync<AmazonChild>(string.Format(url, await amazon.GetMetadataUrl(), id));
+            return result;
+        }
+
+        public async Task<IList<AmazonChild>> GetChildren(string id = null)
+        {
+            if (id == null) id = (await GetRoot()).id;
+            var url = "{0}/nodes/{1}/children";
+            var children = await json.GetJsonAsync<Children>(string.Format(url, await amazon.GetMetadataUrl(), id));
+            return children.data;
+        }
+
+        readonly static Regex filterEscapeChars = new Regex("[ \\+\\-&|!(){}[\\]^'\"~\\*\\?:\\\\]");
+        private string MakeNameFilter(string name)
+        {
+            return "name:" + filterEscapeChars.Replace(name, "\\$0");
+        }
+
+        private string MakeParentFilter(string id)
+        {
+            return "parents:" + id;
+        }
+
+        public async Task<AmazonChild> GetChild(string parentid, string name)
+        {
+            if (parentid == null) parentid = (await GetRoot()).id;
+            var url = string.Format("{0}/nodes?filters={1} AND {2}", await amazon.GetMetadataUrl(), MakeParentFilter(parentid), MakeNameFilter(name));
+            var result = await json.GetJsonAsync<Children>(url);
+            if (result.count == 0) return null;
+            return result.data[0];
+        }
+
+        AmazonChild root;
+        private async Task<AmazonChild> GetRoot()
+        {
+            if (root != null) return root;
+
+            var url = "{0}/nodes?filters=isRoot:true";
+            var result = await json.GetJsonAsync<Children>(string.Format(url, await amazon.GetMetadataUrl()));
+            if (result.count == 0) return null;
+            return result.data[0];
+        }
+    }
+}
--- Readme.md
+++ Readme.md
@@ -3,6 +3,10 @@ ACD Dokan
 
 [Dokan.NET](https://github.com/dokan-dev/dokan-dotnet) based driver for Amazon Cloud Drive. In other words Amazon Cloud Drive as disk drive in Windows.
 
+Prerequisites
+-------------
+[Dokany](https://github.com/dokan-dev/dokany/releases) is required.
+
 Warning
 -------
-May crush your PC and really it should not, but damage files in your Amazon Cloud Drive.
+As it's say Gamma version it may crush together with your OS and/or (really it should not, but...) damage files in your Amazon Cloud Drive.
