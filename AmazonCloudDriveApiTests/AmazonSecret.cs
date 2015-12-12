1a7c935cb740ffdffbb96bb7632a231c9107143d
Rambalac <rambalac+github@gmail.com>
2015-12-12 21:34:28 +0900

    Added sample AmazonSecret for testing

---
+++ AmazonCloudDriveApiTests/AmazonSecret.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azi.Amazon.CloudDrive.Tests
+{
+    public class AmazonSecret
+    {
+        public static readonly string clientId="your id here"!;
+        public static readonly string clientSecret="your secret here"!;
+    }
+}
