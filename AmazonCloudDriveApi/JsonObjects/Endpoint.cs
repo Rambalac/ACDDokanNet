802643584f04f984e41466adc535d8d5bd478a6a
Rambalac <rambalac+github@gmail.com>
2015-12-21 17:38:01 +0900

    Fixed disk used space calculations

--- AmazonCloudDriveApi/JsonObjects/Endpoint.cs
+++ AmazonCloudDriveApi/JsonObjects/Endpoint.cs
@@ -9,32 +9,4 @@ namespace Azi.Amazon.CloudDrive.JsonObjects
         public string contentUrl { get; set; }
         public string metadataUrl { get; set; }
     }
-
-    public class Quota
-    {
-        public long quota { get; set; }
-        public DateTime lastCalculated { get; set; }
-        public long available { get; set; }
-    }
-
-    public class Usage
-    {
-        public class TotalBillable
-        {
-            public class Amount
-            {
-                public int bytes { get; set; }
-                public int count { get; set; }
-            }
-
-            public Amount total { get; set; }
-            public Amount billable { get; set; }
-        }
-
-        public DateTime lastCalculated { get; set; }
-        public TotalBillable other { get; set; }
-        public TotalBillable doc { get; set; }
-        public TotalBillable photo { get; set; }
-        public TotalBillable video { get; set; }
-    }
 }
