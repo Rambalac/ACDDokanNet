be1a2d07411eea918b672c8491c20899797a3fc2
Rambalac <rambalac+github@gmail.com>
2016-01-07 14:24:12 +0900

    Cleaned up repo. Delete Amazon Cloud Drive API to use it as submodule

--- AmazonCloudDriveApiTests/AmazonSecret.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Azi.Amazon.CloudDrive.Tests
-{
-    public static class AmazonSecret
-    {
-        public static string clientId= "amzn1.application-oa2-client.ce45fa54a8ec4daeb7f082343fa1e8d8";
-        public static string clientSecret = "59770303462308f392b73f4f0c68670e611b1da98e94521dd48dcb95458abb9d";
-
-    }
-}
