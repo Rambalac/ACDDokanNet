bf19c561f3700c061923113768ac893cb4124ea2
Rambalac <rambalac+github@gmail.com>
2016-10-24 15:50:03 +0900

    Updated dependencies

--- amazon-clouddrive-dokan/VirtualDriveWrapper.cs
+++ amazon-clouddrive-dokan/VirtualDriveWrapper.cs
@@ -5,36 +5,8 @@
     using System.Diagnostics.Contracts;
     using System.Linq;
     using global::DokanNet;
-    using Tools;
     using global::DokanNet.Logging;
-
-    public class DokanLogger : ILogger
-    {
-        public void Debug(string message, params object[] args)
-        {
-            Log.Trace(message, Log.Dokan);
-        }
-
-        public void Error(string message, params object[] args)
-        {
-            Log.Error(message, Log.Dokan);
-        }
-
-        public void Fatal(string message, params object[] args)
-        {
-            Log.Error(message, Log.Dokan);
-        }
-
-        public void Info(string message, params object[] args)
-        {
-            Log.Info(message, Log.Dokan);
-        }
-
-        public void Warn(string message, params object[] args)
-        {
-            Log.Warn(message, Log.Dokan);
-        }
-    }
+    using Tools;
 
     public class VirtualDriveWrapper
     {
