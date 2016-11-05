d18646d92b3960c008a1b1ec76a44d38fc39458a
Rambalac <rambalac+github@gmail.com>
2016-11-05 10:21:28 +0900

    Revert "Downgraded back to Dokan 0.8.0"
    This reverts commit 7cd632080436b6fa498d228fd05cb45f870e5ac9.

--- amazon-clouddrive-dokan/VirtualDriveWrapper.cs
+++ amazon-clouddrive-dokan/VirtualDriveWrapper.cs
@@ -49,9 +49,9 @@
                 virtualDrive.MountPath = letter + ":\\";
                 mountLetter = letter;
 #if DEBUG
-                virtualDrive.Mount(virtualDrive.MountPath, DokanOptions.DebugMode | DokanOptions.AltStream | DokanOptions.FixedDrive, 0, 800, TimeSpan.FromSeconds(30));
+                virtualDrive.Mount(virtualDrive.MountPath, DokanOptions.DebugMode | DokanOptions.AltStream | DokanOptions.FixedDrive, 0, 800, TimeSpan.FromSeconds(30), new DokanLogger());
 #else
-                virtualDrive.Mount(virtualDrive.MountPath, DokanOptions.AltStream | DokanOptions.FixedDrive, 0, 1000, TimeSpan.FromSeconds(30));
+                virtualDrive.Mount(virtualDrive.MountPath, DokanOptions.AltStream | DokanOptions.FixedDrive, 0, 1000, TimeSpan.FromSeconds(30), new DokanLogger());
 #endif
             }
             catch (DokanException e)
